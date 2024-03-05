63db2e283ffed660a30c46ed55564697c28eafb3
Prasidh Aggarwal <71028669+prasidh-agg@users.noreply.github.com>
2024-03-04 21:54:35 -0700

    updated according to new portfolio

--- resume-sync.sh
+++ resume-sync.sh
@@ -2,18 +2,18 @@
 
 # Names of the resume files
 local_resume="Resume_PrasidhAgg_fs.pdf"
-github_resume="Prasidh-Aggarwal-Resume.pdf"
+github_resume="prasidh-agg-resume.pdf"
 
 # Paths to the resume files
 local_resume_path="/Users/titanium/Desktop/applications/current-resumes/$local_resume"
-github_resume_path="/Users/titanium/Documents/portfolio/prasidh-agg.github.io/public/files/$github_resume"
+github_resume_path="/Users/titanium/Documents/astro-portfolio/public/$github_resume"
 
 # Function to perform git actions
 perform_git_actions() {
     # Commit and push changes
     current_date=$(date +"%Y-%m-%d")
 
-    cd /Users/titanium/Documents/portfolio/prasidh-agg.github.io
+    cd /Users/titanium/Documents/astro-portfolio
     git add "$github_resume_path"
     git commit -m "updated resume on $current_date"
     git push origin main
